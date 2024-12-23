484bc54160e68205187d252e583a737f043b34cc
Hacki <69818162+Hacki01@users.noreply.github.com>
2024-12-23 12:36:53 +0100

    Removed unneeded content

--- app/error.tsx
+++ app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@nextui-org/button";
 import { useEffect } from "react";
 
 export default function Error({
@@ -16,16 +17,17 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center space-y-4">
       <h2>Something went wrong!</h2>
-      <button
+      <Button
+        color="danger"
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
         Try again
-      </button>
+      </Button>
     </div>
   );
 }
