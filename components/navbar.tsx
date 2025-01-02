c298dd9212b17592f44a615e779583bc5e7992b9
Hacki <hackk0110@gmail.com>
2025-01-02 20:39:31 +0100

    removed content

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,44 +6,36 @@ import {
   NavbarBrand,
   NavbarItem,
 } from "@nextui-org/navbar";
-import { Kbd } from "@nextui-org/kbd";
-import { Input } from "@nextui-org/input";
 import NextLink from "next/link";
 
 import { ThemeSwitch } from "@/components/theme-switch";
-import { SearchIcon, Logo } from "@/components/icons";
+import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
             <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <p className="font-bold text-inherit">TimeXchange</p>
           </NextLink>
         </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="center"
+      >
+        <NavbarItem>
+          <NextLink href="/services">Services</NextLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NextLink href="/exchange">Exchange</NextLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NextLink href="/profile">Profile</NextLink>
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent
@@ -53,14 +45,22 @@ export const Navbar = () => {
         <NavbarItem className="hidden sm:flex gap-2">
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
       </NavbarContent>
-
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
         <ThemeSwitch />
         <NavbarMenuToggle />
       </NavbarContent>
-      <NavbarMenu>{searchInput}</NavbarMenu>
+      <NavbarMenu>
+        <NavbarItem>
+          <NextLink href="/services">Services</NextLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NextLink href="/exchange">Exchange</NextLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NextLink href="/profile">Profile</NextLink>
+        </NavbarItem>
+      </NavbarMenu>
     </NextUINavbar>
   );
 };
