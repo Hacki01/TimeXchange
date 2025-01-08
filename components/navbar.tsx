fb29f51e17e80f6da7f150d5f71213920e88120b
Hacki <69818162+Hacki01@users.noreply.github.com>
2025-01-08 19:08:04 +0100

    New navbar idea

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,66 +1,80 @@
-import {
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-} from "@nextui-org/navbar";
-import { Kbd } from "@nextui-org/kbd";
-import { Input } from "@nextui-org/input";
-import NextLink from "next/link";
-
-import { ThemeSwitch } from "@/components/theme-switch";
-import { SearchIcon, Logo } from "@/components/icons";
+"use client";
+import React, { useState } from 'react';
+import { Navbar as NextUINavbar, NavbarBrand, NavbarContent, NavbarItem, Button, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react';
+import NextLink from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const nFormat = new Intl.NumberFormat("pl", {minimumFractionDigits: 2});
+  const TimeCurrency = 513000
+  const FormattedTimeCurrency = nFormat.format(TimeCurrency).replace(',', '.');;
 
   return (
-    <NextUINavbar maxWidth="xl" position="sticky">
+    <NextUINavbar maxWidth="xl" position="sticky" onMenuOpenChange={setIsMenuOpen}>
+      {/* Brand Section */}
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            <div className="flex items-center gap-2">
+              {/* Replace with your Logo component */}
+              <span className="font-bold text-lg">⏳</span>
+              <p className="font-bold text-inherit">TimeXchange</p>
+            </div>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden sm:flex basis-1/5 sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
+      {/* Links Section */}
+      <NavbarContent className="hidden sm:flex basis-3/5 sm:basis-full" justify="center">
+        <NavbarItem>
+          <NextLink href="#home">Home</NextLink>
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        <NavbarItem>
+          <NextLink href="#services">Services</NextLink>
+        </NavbarItem>
+        <NavbarItem>
+          <NextLink href="#faq">FAQ</NextLink>
+        </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <ThemeSwitch />
-        <NavbarMenuToggle />
+      {/* User Actions and Currency Display */}
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="end">
+        <NavbarItem className='flex gap-2 items-center'>
+            <FontAwesomeIcon icon={faClock} />
+            <div className="text-blue-400 mr-5">{FormattedTimeCurrency}</div>
+        </NavbarItem>
+        <NavbarItem className="hidden sm:flex">
+          <Button href="#login">Login</Button>
+        </NavbarItem>
+        <NavbarItem  className="hidden sm:flex">
+          <Button color="primary" href="#signup">Sign Up</Button>
+        </NavbarItem>
       </NavbarContent>
-      <NavbarMenu>{searchInput}</NavbarMenu>
+
+      {/* Mobile Menu */}
+      <NavbarMenu className='gap-4'>
+        <NavbarMenuItem>
+          <NextLink href="#home">Home</NextLink>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <NextLink href="#services">Services</NextLink>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <NextLink href="#faq">FAQ</NextLink>
+        </NavbarMenuItem>
+        <NavbarMenuItem className='mt-6 text-blue-300'>
+          <NextLink href="#login">Login</NextLink>
+        </NavbarMenuItem>
+        <NavbarMenuItem>
+          <NextLink className="text-blue-500" href="#signup">Sign Up</NextLink>
+        </NavbarMenuItem>
+      </NavbarMenu>
     </NextUINavbar>
   );
 };
